2f8d7c3323e574c5dbbd0d9f77ba9d9eb6cb2c24
Lucas de Melo <lucasluandemelo@gmail.com>
2021-01-25 23:46:33 -0300

    Ajuste para background ocupar toda a altura

--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const QuizBackground = styled.div`
   width: 100%;
+  height: 100%;
   background-size: cover;
   background-position: center;
   background-image: url(${({ backgroundImage }) => backgroundImage});
